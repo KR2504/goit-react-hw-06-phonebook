eddd7e63249c8a0696947fb4dd98f470324b261a
Kirill Rumiezhak <kirill.rumiezhak@ukr.net>
2022-08-16 22:41:49 +0300

    finish-hw-6

---
+++ src/components/ContactsList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteContact } from "../redux/contacts";
+
+const Contacts = () => {
+    const contacts = useSelector(state => state.contacts.items);
+    const filter = useSelector(state => state.filter.value);
+
+    const filteredContacts = filter ? contacts.filter(({name}) =>
+        name.toLowerCase().includes(filter.toLowerCase())) : contacts;
+    
+    const dispatch = useDispatch();
+    
+    return (
+        <ul>
+            {filteredContacts.map(({ name, id, number }) => {
+                return (
+                    <li key={id}>{name}: {number} <button onClick={() => dispatch(deleteContact(id))}>Delete</button></li>
+                )
+            })}
+        </ul>
+    );
+};
+
+export default Contacts;
